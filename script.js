b1ace2f542798696b7e9cef5d17322b01d6a3d1f
Martin Fonseca <martin.0521.fonseca@gmail.com>
2025-07-13 12:36:53 -0300

    Cambios

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const form = document.getElementById("formulario");
 const resumen = document.getElementById("resumen");
 
 function guardarEnStorage(nombre, cursosSeleccionados) {
-  cursosSeleccionados.forEach(curso => {
+  cursosSeleccionados.forEach((curso) => {
     const nuevaInscripcion = {
       nombre,
       curso: curso.nombre,
@@ -37,12 +37,23 @@ function guardarEnStorage(nombre, cursosSeleccionados) {
 }
 
 function renderizarResumen() {
-  resumen.innerHTML = "<h2>Inscripciones:</h2>";
+  resumen.innerHTML = "<h2> Inscripciones:</2>";
+  let total = 0;
+
   inscripciones.forEach((item) => {
     const div = document.createElement("div");
     div.textContent = `${item.nombre} - ${item.curso} ($${item.precio})`;
     resumen.appendChild(div);
+    total += item.precio;
   });
+
+  const totalDiv = document.createElement("div");
+  totalDiv.innerHTML = `
+<hr>
+<p><strong>Total a papar:</strong> $${total}</p>
+<p><strong>!Gracias por su compra!</strong></p>
+`;
+  resumen.appendChild(totalDiv);
 }
 
 form.addEventListener("submit", (e) => {
@@ -51,10 +62,16 @@ form.addEventListener("submit", (e) => {
   const nombre = document.getElementById("nombre").value.trim();
 
   // Obtener todos los cursos seleccionados del select múltiple
-  const opcionesSeleccionadas = Array.from(document.getElementById("curso").selectedOptions);
-  const idsCursos = opcionesSeleccionadas.map(option => parseInt(option.value));
+  const opcionesSeleccionadas = Array.from(
+    document.getElementById("curso").selectedOptions
+  );
+  const idsCursos = opcionesSeleccionadas.map((option) =>
+    parseInt(option.value)
+  );
 
-  const cursosSeleccionados = cursos.filter(curso => idsCursos.includes(curso.id));
+  const cursosSeleccionados = cursos.filter((curso) =>
+    idsCursos.includes(curso.id)
+  );
 
   if (!nombre || cursosSeleccionados.length === 0) {
     alert("Por favor completá tu nombre y seleccioná al menos un curso.");
